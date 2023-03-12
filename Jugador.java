1e5bcdbbdf97f206fd07f6c8350c71cd97691688
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-03-12 16:02:41 +0100

    Ejercicio 5

--- Jugador.java
+++ Jugador.java
@@ -1,4 +1,22 @@
 public class Jugador extends Entidad{
     private int experiencia;
     private int nivel;
+
+public int turno(int accion) {
+    int result = 0;
+    switch (accion) {
+        case 0:
+            result = getAtaque()*nivel;
+            return result;
+
+        case 1:
+            result = getDefensa()*nivel;
+            return result;
+
+        //Si no coincide con ninguna de las anteriores devolvemos el valor sin modificaciones
+
+        default:
+            return result;
+        }
+    }
 }
