2a12d423e5195950c49a142bff8ca10ce3b9cb34
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-03-12 16:06:32 +0100

    Ejercicio 6

--- Enemigo.java
+++ Enemigo.java
@@ -1,3 +1,19 @@
 public class Enemigo extends Entidad{
     private int dificultad;
+
+public int turno(int accion) {
+    int result = 0;
+    switch (accion) {
+        case 0:
+            result = getAtaque()*dificultad;
+            return result;
+
+        case 1:
+            result = getAtaque()/2;
+            return result;
+
+        default:
+            return result;
+        }
+    }
 }
