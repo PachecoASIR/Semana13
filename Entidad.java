ef0f2534cf8e75284d2398d894a49600f23e3463
PachecoASIR <jferradaspacheco@danielcastelao.org>
2023-03-12 15:54:58 +0100

    Ejercicio 4

--- Entidad.java
+++ Entidad.java
@@ -3,4 +3,44 @@ public class Entidad {
     private int ataque;
     private int defensa;
     private String nombre;
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida() {
+        this.vida=vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque() {
+        this.ataque=ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa() {
+        this.defensa=defensa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre() {
+        this.nombre=nombre;
+    }
+
+    public int turno (int accion) {
+        int result=0;
+        return result;
+    }
+
+    public void info() {
+    }
 }
